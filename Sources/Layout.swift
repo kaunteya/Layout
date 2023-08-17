196121c69e017e67c3baae58eb990deebbf0e76a
Kaunteya Suryawanshi <k.suryawanshi@gmail.com>
2023-08-17 16:45:25 +0530

    Functions made public

--- Sources/Layout.swift
+++ Sources/Layout.swift
@@ -6,7 +6,7 @@
 
 import AppKit
 
-class Layout {
+public class Layout {
     private let v: NSView
         // OtherView is nil because size constraints dont need it
     private let otherView: NSView?
@@ -17,103 +17,103 @@ class Layout {
     }
 
     var constraints = [NSLayoutConstraint]()
-    @discardableResult func top(_ i: CGFloat = 0) -> Self {
+    @discardableResult public func top(_ i: CGFloat = 0) -> Self {
         constraints.append(v.topAnchor.constraint(equalTo: otherView!.topAnchor, constant: i))
         return self
     }
 
-    @discardableResult func right(_ i: CGFloat = 0) -> Self {
+    @discardableResult public func right(_ i: CGFloat = 0) -> Self {
         constraints.append(v.rightAnchor.constraint(equalTo: otherView!.rightAnchor, constant: i))
         return self
     }
 
-    @discardableResult func bottom(_ i: CGFloat = 0) -> Self {
+    @discardableResult public func bottom(_ i: CGFloat = 0) -> Self {
         constraints.append(v.bottomAnchor.constraint(equalTo: otherView!.bottomAnchor, constant: i))
         return self
     }
 
-    @discardableResult func bottomSafeArea(_ i: CGFloat = 0) -> Self {
+    @discardableResult public func bottomSafeArea(_ i: CGFloat = 0) -> Self {
         constraints.append(v.bottomAnchor.constraint(equalTo: otherView!.layoutMarginsGuide.bottomAnchor, constant: i))
         return self
     }
 
-    @discardableResult func left(_ i: CGFloat = 0) -> Self {
+    @discardableResult public func left(_ i: CGFloat = 0) -> Self {
         constraints.append(v.leftAnchor.constraint(equalTo: otherView!.leftAnchor, constant: i))
         return self
     }
 
-    @discardableResult func trailing(_ i: CGFloat = 0) -> Self {
+    @discardableResult public func trailing(_ i: CGFloat = 0) -> Self {
         constraints.append(v.trailingAnchor.constraint(equalTo: otherView!.trailingAnchor, constant: i))
         return self
     }
 
-    @discardableResult func leading(_ i: CGFloat = 0) -> Self {
+    @discardableResult public func leading(_ i: CGFloat = 0) -> Self {
         constraints.append(v.leadingAnchor.constraint(equalTo: otherView!.leadingAnchor, constant: i))
         return self
     }
 
-    @discardableResult func width(_ i: CGFloat = 0) -> Self {
+    @discardableResult public func width(_ i: CGFloat = 0) -> Self {
         constraints.append(v.widthAnchor.constraint(equalToConstant: i))
         return self
     }
 
-    @discardableResult func centerX(_ i: CGFloat = 0) -> Self {
+    @discardableResult public func centerX(_ i: CGFloat = 0) -> Self {
         constraints.append(v.centerXAnchor.constraint(equalTo: otherView!.centerXAnchor, constant: i))
         return self
     }
 
-    @discardableResult func centerY(_ i: CGFloat = 0) -> Self {
+    @discardableResult public func centerY(_ i: CGFloat = 0) -> Self {
         constraints.append(v.centerYAnchor.constraint(equalTo: otherView!.centerYAnchor, constant: i))
         return self
     }
 
-    @discardableResult func center() -> Self {
+    @discardableResult public func center() -> Self {
         self.centerX(0).centerY(0)
     }
 
-    @discardableResult func height(_ i: CGFloat) -> Self {
+    @discardableResult public func height(_ i: CGFloat) -> Self {
         constraints.append(v.heightAnchor.constraint(equalToConstant: i))
         return self
     }
 
-    @discardableResult func size(_ i: CGFloat) -> Self {
+    @discardableResult public func size(_ i: CGFloat) -> Self {
         self.width(i).height(i)
         return self
     }
 
         /// Adds self.left(i).right(-i)
-    @discardableResult func horizontal(_ i: CGFloat = 0) -> Self {
+    @discardableResult public func horizontal(_ i: CGFloat = 0) -> Self {
         self.left(i).right(-i)
     }
 
         /// Adds self.top(i).bottom(-i)
-    @discardableResult func vertical(_ i: CGFloat = 0) -> Self {
+    @discardableResult public func vertical(_ i: CGFloat = 0) -> Self {
         self.top(i).bottom(-i)
     }
 
         /// Other view can be superview
-    @discardableResult func equalViews(_ i: CGFloat = 0) -> Self {
+    @discardableResult public func equalViews(_ i: CGFloat = 0) -> Self {
         self.top(i).right(-i).bottom(-i).left(i)
     }
 }
 
 extension NSView {
-    func applyConstraints(layout: Layout) {
+    public func applyConstraints(layout: Layout) {
         assert(translatesAutoresizingMaskIntoConstraints == false)
         NSLayoutConstraint.activate(layout.constraints)
     }
 
-    func applyConstraints(layout: (Layout) -> Layout) {
+    public func applyConstraints(layout: (Layout) -> Layout) {
         applyConstraints(layout: layout(Layout(self)))
     }
 
-    func addSubview(_ v: NSView, layout: (Layout) -> Layout) {
+    public func addSubview(_ v: NSView, layout: (Layout) -> Layout) {
         addSubview(v)
         let edges = layout(Layout(v))
         NSLayoutConstraint.activate(edges.constraints)
     }
 
-    func addSubview(_ v: NSView, handler: () -> Layout) {
+    public func addSubview(_ v: NSView, handler: () -> Layout) {
         addSubview(v)
         let edges = handler()
         NSLayoutConstraint.activate(edges.constraints)
